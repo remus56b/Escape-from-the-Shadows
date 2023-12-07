2ff87aabd0dbc58c14426b10e3a952252404c61c
remus56b <remusbacaoanu@yahoo.com>
2023-12-07 21:04:30 +0200

    adding key as rigid body

---
+++ basement-scene/IMR-main/Assets/animationStateController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]
+    HandsManager handsManager; // Adaugă acest câmp pentru a face referire la HandsManager
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (handsManager != null)
+        {
+            bool isGrabbing = handsManager.IsGrabbing();
+
+            if (isGrabbing)
+            {
+                bool hasGrabbedKey = CheckIfPlayerGrabbedKey();
+
+                if (hasGrabbedKey)
+                {
+                    animator.SetBool("isJumping", true);
+                }
+            }
+        }
+    }
+
+    private bool CheckIfPlayerGrabbedKey()
+    {
+        // Presupunem că avem un obiect numit "rust_key" în scena
+        GameObject key = GameObject.Find("rust_key");
+
+        if (key == null)
+        {
+            // Dacă nu există un obiect numit "rust_key", returnează false
+            return false;
+        }
+
+        // Verifică dacă jucătorul a intrat în coliziune cu cheia
+        Collider keyCollider = key.GetComponent<Collider>();
+        Collider playerCollider = GetComponent<Collider>();
+
+        if (keyCollider.bounds.Intersects(playerCollider.bounds))
+        {
+            // Dacă jucătorul a intrat în coliziune cu cheia, returnează true
+            return true;
+        }
+
+        // Dacă jucătorul nu a intrat în coliziune cu cheia, returnează false
+        return false;
+    }
+}
