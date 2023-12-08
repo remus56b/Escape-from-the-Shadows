538c138f8bdadc664c67ee04a4fd36a40d63740a
cosminalex2002 <murarucosminalex@gmail.com>
2023-12-08 12:43:41 +0200

    added outside sounds

---
+++ basement-scene/IMR-main/Assets/outside_sounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outside_sounds : MonoBehaviour
+{
+    public AudioSource[] audioSources; 
+
+    void Start()
+    {
+        if (audioSources.Length == 3) 
+        {
+            StartCoroutine(PlayContinuousSounds());
+        }
+        else
+        {
+            Debug.LogError("Trebuie să aveți exact 3 surse audio în vectorul audioSources!");
+        }
+    }
+
+    IEnumerator PlayContinuousSounds()
+    {
+        while (true)
+        {
+            foreach (AudioSource audioSource in audioSources)
+            {
+                audioSource.Play();
+
+                yield return new WaitForSeconds(audioSource.clip.length);
+            }
+        }
+    }
+
+    void Update()
+    {
+    }
+}
