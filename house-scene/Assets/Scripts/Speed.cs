db61613431b12016985f3f4227b0b60037c7c8fb
georgecernat77 <george.cernat07@gmail.com>
2023-12-15 14:50:55 +0200

    adding collision to objects from house scene and player improved movement
    -the player now walks faster
    -fixed some bugs related to xr toolkit
    -collision to more objects added

---
+++ house-scene/Assets/Scripts/Speed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+    public float viteza = 5f; // Viteza obiectului
+
+    void Update()
+    {
+        // Inițializează o variabilă pentru direcția de deplasare
+        Vector3 directieDeplasare = Vector3.zero;
+
+        // Verifică tastele pentru a seta direcția de deplasare
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.G))
+        {
+            directieDeplasare += transform.forward; // Înainte
+        }
+
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.G))
+        {
+            directieDeplasare -= transform.forward; // Înapoi
+        }
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.G))
+        {
+            directieDeplasare -= transform.right; // Stânga
+        }
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.G))
+        {
+            directieDeplasare += transform.right; // Dreapta
+        }
+
+        // Normalizează direcția de deplasare pentru a menține viteza constantă în toate direcțiile
+        if (directieDeplasare != Vector3.zero)
+        {
+            directieDeplasare = directieDeplasare.normalized;
+        }
+
+        // Aplică mișcarea în direcția specificată
+        transform.Translate(directieDeplasare * viteza * Time.deltaTime);
+
+        // Alte funcții sau logici pentru mișcarea obiectului pot fi adăugate aici în funcție de necesități
+    }
+}
