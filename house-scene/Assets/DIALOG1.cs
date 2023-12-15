b0df453b954a25db033abdc55a3848758a69a6b3
cosminalex2002 <murarucosminalex@gmail.com>
2023-12-15 19:25:30 +0200

    added dialogues

---
+++ house-scene/Assets/DIALOG1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIALOG1 : MonoBehaviour
+{
+    public AudioSource sound1; // Prima sursă audio
+    public AudioSource sound2; // A doua sursă audio
+    public AudioSource sound3; // A treia sursă audio
+    public AudioSource sound4; // A patra sursă audio
+    public AudioSource sound5; // A cincea sursă audio
+    public AudioSource sound6; // A șasea sursă audio
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlaySounds());
+    }
+
+    IEnumerator PlaySounds()
+    {
+        yield return new WaitForSeconds(2f); // Așteaptă 3 secunde
+
+        sound1.Play(); // Pornește primul sunet
+
+        yield return new WaitForSeconds(sound1.clip.length + 2f); // Așteaptă lungimea sunetului 1 + 3 secunde
+
+        sound2.Play(); // Pornește al doilea sunet după ce se termină primul sunet
+
+        yield return new WaitForSeconds(sound2.clip.length + 2f); // Așteaptă lungimea sunetului 2 + 2 secunde
+
+        sound3.Play(); // Pornește al treilea sunet după ce se termină al doilea sunet
+
+        yield return new WaitForSeconds(sound3.clip.length + 2f); // Așteaptă lungimea sunetului 3 + 3 secunde
+
+        sound4.Play(); // Pornește al patrulea sunet după ce se termină al treilea sunet
+
+        yield return new WaitForSeconds(sound4.clip.length + 2f); // Așteaptă lungimea sunetului 4 + 2 secunde
+
+        sound5.Play(); // Pornește al cincilea sunet după ce se termină al patrulea sunet
+
+        yield return new WaitForSeconds(sound5.clip.length + 2f); // Așteaptă lungimea sunetului 5 + 2 secunde
+
+        sound6.Play(); // Pornește al șaselea sunet după ce se termină al cincilea sunet
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- house-scene/Assets/Scripts/HandsManager.cs
+++ house-scene/Assets/Scripts/HandsManager.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandsManager : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     Animator leftHand;
     [SerializeField]
     Animator rightHand;
+
+    public float viteza = 5f; // Viteza obiectului
 
     private Vector3 throwDirection;
     private float throwForce = 8.0f;
@@ -16,18 +18,66 @@ public class HandsManager : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+        HandleGrabbing();
+        HandleThrowing();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 directieDeplasare = Vector3.zero;
+
+        bool apasatInainte = Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S);
+        bool apasatInapoi = Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W);
+        bool apasatStanga = Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D);
+        bool apasatDreapta = Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A);
+
+        if (apasatInainte)
+        {
+            directieDeplasare += transform.forward; // Înainte
+        }
+        else if (apasatInapoi)
+        {
+            directieDeplasare -= transform.forward; // Înapoi
+        }
+
+        if (apasatStanga)
+        {
+            directieDeplasare -= transform.right; // Stânga
+        }
+        else if (apasatDreapta)
+        {
+            directieDeplasare += transform.right; // Dreapta
+        }
+
+        if (directieDeplasare != Vector3.zero)
+        {
+            directieDeplasare = directieDeplasare.normalized;
+        }
+
+        transform.Translate(directieDeplasare * viteza * Time.deltaTime, Space.Self);
+    }
+
+
+
+    private void HandleGrabbing()
+    {
         isGrabbing = Input.GetKey(KeyCode.G);
         SetGrabbingTrigger(isGrabbing);
+    }
 
+    private void HandleThrowing()
+    {
         if (Input.GetKeyDown(KeyCode.Y))
         {
             CalculateThrowDirection();
         }
     }
 
-    public bool IsGrabbing()
+    private void SetGrabbingTrigger(bool isGrabbing)
     {
-        return isGrabbing;
+        leftHand.SetBool("isGrabbing", isGrabbing);
+        rightHand.SetBool("isGrabbing", isGrabbing);
     }
 
     private void CalculateThrowDirection()
@@ -35,11 +85,5 @@ public class HandsManager : MonoBehaviour
         Vector3 forwardDirection = Camera.main.transform.forward;
         Vector3 upwardDirection = Vector3.up * throwHeight;
         throwDirection = forwardDirection + upwardDirection;
-    }
-
-    private void SetGrabbingTrigger(bool isGrabbing)
-    {
-        leftHand.SetBool("isGrabbing", isGrabbing);
-        rightHand.SetBool("isGrabbing", isGrabbing);
     }
 }
