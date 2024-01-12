6fd61af9adf4714914f60d927d629efca132bf67
cosminalex2002 <murarucosminalex@gmail.com>
2024-01-12 14:56:29 +0200

    added cemetery

---
+++ house-scene/Assets/PortalTeleport2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport2 : MonoBehaviour
+{
+    public Transform teleportDestination;
+    public Transform player;
+    private GameObject doorObject;
+    private Animation doorAnimation;
+
+
+    private void Start()
+    {
+        // Găsește obiectul ușii bazat pe tag
+        doorObject = GameObject.FindGameObjectWithTag("door3");
+    }
+
+    private void Update()
+    {
+        GameObject destination3 = GameObject.Find("destination3");
+
+        Vector3 portalPosition = transform.position;
+        Vector3 playerPosition = player.position;
+
+
+
+        float distance = Vector3.Distance(portalPosition, playerPosition);
+        if (distance < 6f)
+        {
+            if (destination3 != null)
+            {
+                player.position = destination3.transform.position;
+
+            }
+        }
+    }
+}
