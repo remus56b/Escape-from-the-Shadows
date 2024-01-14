17c887d58f413f4e532c074135d0b6e0456514d2
georgecernat77 <george.cernat07@gmail.com>
2024-01-14 13:28:33 +0200

    added trigger for chest animation
    -when the player has the key and it is close to the chest, it opens

---
+++ house-scene/Assets/ChestController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChestController : MonoBehaviour
+{
+    public GameObject rustKey; // Obiectul "rust_key"
+    public Animator chestAnimator; // Animatorul atașat obiectului "chest"
+    public float triggerDistance = 1.0f; // Distanța la care se declanșează animația
+
+    void Update()
+    {
+        // Calculează distanța dintre "chest" și "rust_key"
+        float distance = Vector3.Distance(transform.position, rustKey.transform.position);
+
+        // Dacă "rust_key" este suficient de aproape, declanșează animația
+        if (distance <= triggerDistance)
+        {
+            chestAnimator.SetTrigger("OpenChest");
+            Debug.Log("Animația de deschidere a cufărului a fost activată."); // Afișează un mesaj în consolă
+        }
+    }
+}
---
+++ house-scene/Assets/chest/playOpenChest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playOpenChest : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject rustKey; // Obiectul "rust_key"
+    public float triggerDistance = 1.0f; // Distanța la care se declanșează animația
+
+    void Update()
+    {
+        // Calculează distanța dintre "chest" și "rust_key"
+        float distance = Vector3.Distance(transform.position, rustKey.transform.position);
+
+        // Dacă "rust_key" este suficient de aproape, setează "openChest" pe true
+        if (distance <= triggerDistance)
+        {
+            animator.SetBool("openChest", true);
+        }
+        else
+        {
+            animator.SetBool("openChest", false);
+        }
+    }
+}
