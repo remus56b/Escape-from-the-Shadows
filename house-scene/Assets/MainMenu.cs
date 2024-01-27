abf0913904aeb1d0577394fdcfcf08fc8a3aeddc
cosminalex2002 <murarucosminalex@gmail.com>
2024-01-27 11:58:43 +0200

    added hp text

---
+++ house-scene/Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Începe jocul; poți să încarci scena principală a jocului
+        SceneManager.LoadScene("SampleScene");
+    }
+}
