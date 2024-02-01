bb25265def9a5db59c952bf260c88f02c39edf90
cosminalex2002 <murarucosminalex@gmail.com>
2024-02-01 15:16:04 +0200

    added flashlight and start button

---
+++ house-scene/Assets/PlayScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        // Începe jocul; poți să încarci scena principală a jocului
+        SceneManager.LoadScene("SampleScene");
+    }
+}
