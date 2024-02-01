a06e0c97f79f28e367fce022ef108c224e24c492
cosminalex2002 <murarucosminalex@gmail.com>
2024-02-01 15:45:00 +0200

    added pause button

---
+++ house-scene/Assets/PauseScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public void PauseButton()
+    {
+        // Începe jocul; poți să încarci scena principală a jocului
+        SceneManager.LoadScene("MainMenu");
+    }
+}
